7b95c69a6dc7e525ef0a2bfa38852bc7d8ac3870
uchetheafrican <emmanueldavidijoma@gmail.com>
2022-09-11 05:59:50 -0700

    4. When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,17 +2,14 @@
 
 /**
  *
- *  * main - print if the number is postive, zero, or negative
+ * main - print if the number is postive, zero, or negative
+ * Description: using the main function 
+ * this program prints "Programming is positive, zero, or negative"
  *
- *   *
+ * Return: 0
+
  *
- *    * Description: using the main function
- *
- *     * this program prints "Programming is positive, zero, or negative
- *
- *      * Return: 0
- *
- *       */
+ */
 
 int main(void)
 
@@ -23,19 +20,15 @@ int main(void)
 	for (ch = 'a' ; ch <= 'z' ; ch++)
 
 	{
-
 		if (ch != 'e' && ch != 'q')
+		
+	{
+		putchar(ch);
 
-		{
-
-				putchar(ch);
-
-		}
+	}
 
 	}
 
 	putchar('\n');
-
 	return (0);
-
 }
